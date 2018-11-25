a616b01389a8b01bf1845c0e1b7bb7c1295a7838
Shane Garrity <shanedgarrity@gmail.com>
2018-11-25 03:28:12 -0500

    bump running instances to 2 for redundancy, balancing load

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
 
     // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
     // args: 'one two',
-    instances: 1,
+    instances: 2,
     autorestart: true,
     watch: false,
     max_memory_restart: '1G',
