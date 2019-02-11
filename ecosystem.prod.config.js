120c377abac5bdcff8015cf2e1de8740a815416d
shane garrity <shanedgarrity@gmail.com>
2019-02-11 01:24:42 -0500

    debug pm2 env selection

--- ecosystem.prod.config.js
+++ ecosystem.prod.config.js
@@ -11,12 +11,12 @@ module.exports = {
       append_env_to_name: true,
       env: {
         NODE_ENV: 'production',
-        DATABASE_HOST: 'postgres-ok',
-        DATABASE_PORT: 5432,
-        DATABASE_NAME: 'strapi-db',
-        DATABASE_USERNAME: 'strapi-user',
-        DATABASE_PASSWORD: 'strapi-user-alright',
-        DATABASE_SSL: false,
+        // DATABASE_HOST: 'postgres-ok',
+        // DATABASE_PORT: 5432,
+        // DATABASE_NAME: 'strapi-db',
+        // DATABASE_USERNAME: 'strapi-user',
+        // DATABASE_PASSWORD: 'strapi-user-alright',
+        // DATABASE_SSL: false,
       },
       env_production: {
         NODE_ENV: 'production',
