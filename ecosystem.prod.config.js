80a8d2678d1a21ffe580fab065b8fcd001686ea9
shane garrity <shanedgarrity@gmail.com>
2019-02-11 01:46:16 -0500

    fix: pm2 env vars cascade, do not set in default if meant to be overridden

--- ecosystem.prod.config.js
+++ ecosystem.prod.config.js
@@ -11,15 +11,6 @@ module.exports = {
       append_env_to_name: true,
       env: {
         NODE_ENV: 'production',
-        DATABASE_HOST: 'postgres-ok',
-        DATABASE_PORT: 5432,
-        DATABASE_NAME: 'strapi-db',
-        DATABASE_USERNAME: 'strapi-user',
-        DATABASE_PASSWORD: 'strapi-user-alright',
-        DATABASE_SSL: false,
-      },
-      env_production: {
-        NODE_ENV: 'production',
       },
       exec_mode: 'cluster',
     },
